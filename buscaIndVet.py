4fe4f165c2e07101707439c9da8368ffaea47fad
kulido <jcvsoares1@gmail.com>
2021-12-14 12:38:52 -0100

    ALgoritmo que busca ind de um elemento caso contrario retorna -1

---
+++ buscaIndVet.py
@@ -0,0 +1,20 @@
+import sys
+def buscaNaVe(nums, target):
+
+    if target in nums:
+        return nums.index(target)
+    else:
+        nums.append(target)
+        nums.sort()
+        print(nums)
+        return nums.index(target)
+
+
+if __name__ == "__main__":
+    input = sys.stdin.readline
+
+    nums = list(map(int, input().split()))
+    target = int(input())
+    k = buscaNaVe(nums, target)
+
+    print(k)
