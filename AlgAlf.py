8918a8a5d8f8caeab1a80ed4f8d890a1deeb4dd7
kulido <jcvsoares1@gmail.com>
2022-01-05 23:49:26 -0100

    Ciclico Alf minimo de passo para chegar ao B carater

---
+++ AlgAlf.py
@@ -0,0 +1,26 @@
+alf = ['a', 'b', 'c', 'd', 'e', 'f',
+       'g', 'h', 'i', 'j', 'k', 'l',
+       'm', 'n', 'o', 'p', 'q', 'r',
+       's', 't', 'u', 'v', 'w', 'x',
+       'y', 'z', 'a', 'b', 'c', 'd',
+       'e', 'f', 'g', 'h', 'i', 'j',
+       'k', 'l', 'm', 'n', 'o', 'p',
+       'q', 'r', 's', 't', 'u', 'v',
+       'w', 'x', 'y', 'z']
+
+ref = 0
+soma = 0
+arr = []
+s = []
+def direito(arr, s):
+    soma = 0
+    som = 0
+    somaf = 0
+    for i in range(0, len(s)):
+        b = alf.index(arr[i])
+
+
+if __name__ == "__main__":
+    arr = list(map(str, input()))
+    s = list(map(str, input()))
+direito(arr, s)
