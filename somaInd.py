99d9479551af8bfcf97dbdedbc1884f45c856951
João Cláudio Soares <59915163+joao-claudio1@users.noreply.github.com>
2022-08-28 01:08:25 -0100

    clone: update somaInd.py

--- somaInd.py
+++ somaInd.py
@@ -1,7 +1,6 @@
 class Solution(object):
     def doisSoma(self, nums, target):
-  
-        lista = []
+          lista = []
         for i in range(len(nums)):
             k = target - nums[i]
             if k in nums and nums.index(k) != i:
